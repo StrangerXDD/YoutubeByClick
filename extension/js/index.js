e45caf0435e6031a4e4ac97934f80872b6fe5431
StrangerXDD <tcheng69@my.bcit.ca>
2022-09-05 22:30:14 -0700

    added bootstrap and chrome manifest

--- extension/js/index.js
+++ extension/js/index.js
@@ -1,28 +1,28 @@
-// This block will add a class to the selected singerTag, so it will highlight the click element but remove the previous click element class
-
-// this block get all the selected tag
-
-fetch("../json/singers.json")
-  .then((response) => response.json())
-  .then((json) => {
-    var allSingerList = json;
-
-    const singerTag = document.querySelectorAll(".singer_tag__item");
-
-    singerTag.forEach((ele) => {
-      ele.addEventListener("click", (event) => {
-        var parent = ele.parentNode;
-        var children = parent.children;
-        for (var i = 0; i < children.length; i++) {
-          var child = children[i];
-          child.classList.contains("singer_tag__item--select");
-          child.classList.remove("singer_tag__item--select");
-        }
-        ele.classList.add("singer_tag__item--select");
-        selectedTags();
-      });
+// singer data store in api npoint
+fetch("https://api.npoint.io/05d24548da9af9b22e77")
+.then((response) => response.json())
+.then((json) => {
+  var allSingerList = json;
+  
+  const singerTag = document.querySelectorAll(".singer_tag__item");
+  
+  // This block will add a class to the selected singerTag, so it will highlight the click element but remove the previous click element class
+  
+  singerTag.forEach((ele) => {
+    ele.addEventListener("click", (event) => {
+      var parent = ele.parentNode;
+      var children = parent.children;
+      for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        child.classList.contains("singer_tag__item--select");
+        child.classList.remove("singer_tag__item--select");
+      }
+      ele.classList.add("singer_tag__item--select");
+      selectedTags();
     });
-
+  });
+  
+  // this block get all the selected tag
     var selectedTags = () => {
       const selectedTag = document.querySelectorAll(
         ".singer_tag__item--select"
